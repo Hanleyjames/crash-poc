78419bdf1371ad34c363a4fc7663437a16e17df6
James Hanley <jahanley@mng-it-lp3597.home>
2021-10-09 13:45:20 -0600

    add soft server

--- api/server.js
+++ api/server.js
@@ -1,1 +1,9 @@
-console.log("Hello");
+var http = require('http');
+
+var server = http.createServer(function(req, res){
+    var date = new Date().toISOString();
+    console.log('sending date: ', date);
+    res.end(date);
+});
+
+server.listen(8000);
